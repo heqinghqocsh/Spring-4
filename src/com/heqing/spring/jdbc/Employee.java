83981ed4610d50ae62709ed3e781198aee380db9
heqinghqocsh <heqinghqocsh@outlook.com>
2015-06-02 15:06:37 +0800

    使用JdbcTemplate 完成数据库的一些列查询以及对象的映射。

---
+++ src/com/heqing/spring/jdbc/Employee.java
@@ -0,0 +1,45 @@
+package com.heqing.spring.jdbc;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private float salary;
+	private String email;
+	private Department dept;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Department getDept() {
+		return dept;
+	}
+	public void setDept(Department dept) {
+		this.dept = dept;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary
+				+ ", emailS=" + email + ", dept=" + dept + "]";
+	}
+}
--- src/com/heqing/spring/jdbc/JDBCTest.java
+++ src/com/heqing/spring/jdbc/JDBCTest.java
@@ -9,7 +9,9 @@ import javax.sql.DataSource;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class JDBCTest {
 
@@ -20,6 +22,47 @@ public class JDBCTest {
 		ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		jdbcTemplate = (JdbcTemplate) ctx.getBean("jdbcTemplate");
 	}
+	/**
+	 * 从数据库获取一条记录，并且得到对应的一个对象
+	 * 1.其中的RowMapper指定如何去映射结果集的行
+	 * 2.使用sql 中列的别名完成列名和类的属性的映射，例如employeeName name
+	 * 3.不支持级联属性，如dept_id as \"dept.id\".
+	 */
+	@Test
+	public void testQueryForObject(){
+		String sql = "SELECT employeeId id,employeeName name,employeeEmail email "
+				+ "FROM employee WHERE employeeId = ?";
+		RowMapper<Employee> rowMapper = new BeanPropertyRowMapper<>(Employee.class);
+		Employee employee = jdbcTemplate.queryForObject(sql, rowMapper, 12);
+		System.out.println(employee);
+	}
+	
+	/**
+	 * 查到实体类的集合
+	 * 注意调用的不是queryforlist（）方法
+	 */
+	@Test
+	public void testQueryForList(){
+		String sql = "SELECT employeeId id,employeeName name,employeeEmail email "
+				+ "FROM employee";
+		RowMapper<Employee> rowMapper = new BeanPropertyRowMapper<>(Employee.class);
+		List<Employee> employees = jdbcTemplate.query(sql, rowMapper);
+		for (Employee employee : employees) {
+			System.out.println(employee);
+		}
+	}
+	
+	/**
+	 * 获取某一个值
+	 */
+	@Test
+	public void testQueryForValue(){
+		String sql = "SELECT count(employeeId) FROM employee";
+		long count = jdbcTemplate.queryForObject(sql, Long.class);
+		System.out.println("一共有记录："+count);
+	}
+	
+	
 	
 	/**
 	 * 批量操作
